5d5ee75f47d44205c02190bdbab6f8cde675c93e
RomanPinelo <prof.roman.pinelo@gmail.com>
2022-01-07 23:19:06 -0600

    casilla llena

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
 var nuPlayer = document.getElementById("numPlayer");
+nuPlayer.innerHTML = '1';
 var casilla11 = document.getElementById("cas1");
 var casilla12 = document.getElementById("cas2");
 var casilla13 = document.getElementById("cas3");
@@ -10,133 +11,190 @@ var casilla32 = document.getElementById("cas8");
 var casilla33 = document.getElementById("cas9");
 var tie = document.getElementById("Tie");
 var turno = true;
-var tiros = 9;
 
-
-function insertaX() {
-  casilla11.addEventListener('click', () => {
-    casilla11.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
-    console.log('casilla 1 llena');
-    turno = false;
-    inicio();
-  });
-  casilla12.addEventListener('click', () => {
-    casilla12.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
-    console.log('casilla 2 llena');
-    turno = false;
-    inicio();
-  });
-  casilla13.addEventListener('click', () => {
-    casilla13.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
-    console.log('casilla 3 llena');
-    turno = false;
-    inicio();
-  });
-  casilla21.addEventListener('click', () => {
-    casilla21.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
-    console.log('casilla 4 llena'); 
-    turno = false;
-    inicio();
-  });
-  casilla22.addEventListener('click', () => {
-    casilla22.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
-    console.log('casilla 5 llena');
-    turno = false;
-    inicio();
-  });
-  casilla23.addEventListener('click', () => {
-    casilla23.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
-    console.log('casilla 6 llena');
-    turno = false;
-    inicio();
-  });
-  casilla31.addEventListener('click', () => {
-    casilla31.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
-    console.log('casilla 7 llena');
-    turno = false;
-    inicio();
-  });
-  casilla32.addEventListener('click', () => {
-    casilla32.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
-    console.log('casilla 8 llena');
-    turno = false;
-    inicio();
-  });
-  casilla33.addEventListener('click', () => {
-    casilla33.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
-    console.log('casilla 9 llena');
-    turno = false;
-    inicio();
-  });
-}
-
-function insertaO() {
-  casilla11.addEventListener('click', () => {
-    casilla11.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
-    console.log('casilla 2 llena');
-    turno = true;
-    inicio();
-  });
-  casilla12.addEventListener('click', () => {
-    casilla12.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
-    console.log('casilla 2 llena');
-    turno = true;
-    inicio();
-  });
-  casilla13.addEventListener('click', () => {
-    casilla13.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
-    console.log('casilla 3 llena');
-    turno = true;
-    inicio();
-  });
-  casilla21.addEventListener('click', () => {
-    casilla21.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
-    console.log('casilla 4 llena');
-    turno = true;
-    inicio();
-  });
-  casilla22.addEventListener('click', () => {
-    casilla22.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
-    console.log('casilla 5 llena');
-    turno = true;
-    inicio();
-  });
-  casilla23.addEventListener('click', () => {
-    casilla23.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
-    console.log('casilla 6 llena');
-    turno = true;
-    inicio();
-  });
-  casilla31.addEventListener('click', () => {
-    casilla31.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
-    console.log('casilla 7 llena');
-    turno = true;
-    inicio();
-  });
-  casilla32.addEventListener('click', () => {
-    casilla32.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
-    console.log('casilla 8 llena');
-    turno = true;
-    inicio();
-  });
-  casilla33.addEventListener('click', () => {
-    casilla33.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
-    console.log('casilla 9 llena');
-    turno = true;
-    inicio();
-  });
-}
 
 function inicio() {
-  if (turno) {
-    nuPlayer.innerHTML = '1';
-    insertaX();
-  } else { 
-    nuPlayer.innerHTML = '2';
-    insertaO();
-  }
+  casilla11.addEventListener('click', () => {
+    if (turno) {
+      let filled = casilla11.hasChildNodes();
+      if (!filled) {
+        casilla11.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
+        console.log('equis');
+        nuPlayer.innerHTML = '2';
+        turno = false;
+      }
+    } else {
+      let filled = casilla11.hasChildNodes();
+      if (!filled) {
+        casilla11.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
+        console.log('circulo');
+        nuPlayer.innerHTML = '1';
+        turno = true;
+      }
+    }
+  });
+
+  casilla12.addEventListener('click', () => {
+    if (turno) {
+      let filled = casilla12.hasChildNodes();
+      if (!filled) {
+        casilla12.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
+        console.log('equis');
+        nuPlayer.innerHTML = '2';
+        turno = false;
+      }
+    } else {
+      let filled = casilla12.hasChildNodes();
+      if (!filled) {
+        casilla12.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
+        console.log('circulo');
+        nuPlayer.innerHTML = '1';
+        turno = true;
+      }
+    }
+  });
+
+  casilla13.addEventListener('click', () => {
+    if (turno) {
+      let filled = casilla13.hasChildNodes();
+      if (!filled) {
+        casilla13.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
+        console.log('equis');
+        nuPlayer.innerHTML = '2';
+        turno = false;
+      }
+    } else {
+      let filled = casilla13.hasChildNodes();
+      if (!filled) {
+        casilla13.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
+        console.log('circulo');
+        nuPlayer.innerHTML = '1';
+        turno = true;
+      }
+    }
+  });
+
+  casilla21.addEventListener('click', () => {
+    if (turno) {
+      let filled = casilla21.hasChildNodes();
+      if (!filled) {
+        casilla21.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
+        console.log('equis');
+        nuPlayer.innerHTML = '2';
+        turno = false;
+      }
+    } else {
+      let filled = casilla21.hasChildNodes();
+      if (!filled) {
+        casilla21.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
+        console.log('circulo');
+        nuPlayer.innerHTML = '1';
+        turno = true;
+      }
+    }
+  });
+
+  casilla22.addEventListener('click', () => {
+    if (turno) {
+      let filled = casilla22.hasChildNodes();
+      if (!filled) {
+        casilla22.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
+        console.log('equis');
+        nuPlayer.innerHTML = '2';
+        turno = false;
+      }
+    } else {
+      let filled = casilla22.hasChildNodes();
+      if (!filled) {
+        casilla22.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
+        console.log('circulo');
+        nuPlayer.innerHTML = '1';
+        turno = true;
+      }
+    }
+  });
+
+  casilla23.addEventListener('click', () => {
+    if (turno) {
+      let filled = casilla23.hasChildNodes();
+      if (!filled) {
+        casilla23.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
+        console.log('equis');
+        nuPlayer.innerHTML = '2';
+        turno = false;
+      }
+    } else {
+      let filled = casilla23.hasChildNodes();
+      if (!filled) {
+        casilla23.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
+        console.log('circulo');
+        nuPlayer.innerHTML = '1';
+        turno = true;
+      }
+    }
+  });
+
+  casilla31.addEventListener('click', () => {
+    if (turno) {
+      let filled = casilla31.hasChildNodes();
+      if (!filled) {
+        casilla31.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
+        console.log('equis');
+        nuPlayer.innerHTML = '2';
+        turno = false;
+      }
+    } else {
+      let filled = casilla31.hasChildNodes();
+      if (!filled) {
+        casilla31.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
+        console.log('circulo');
+        nuPlayer.innerHTML = '1';
+        turno = true;
+      }
+    }
+  });
+
+  casilla32.addEventListener('click', () => {
+    if (turno) {
+      let filled = casilla32.hasChildNodes();
+      if (!filled) {
+        casilla32.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
+        console.log('equis');
+        nuPlayer.innerHTML = '2';
+        turno = false;
+      }
+    } else {
+      let filled = casilla32.hasChildNodes();
+      if (!filled) {
+        casilla32.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
+        console.log('circulo');
+        nuPlayer.innerHTML = '1';
+        turno = true;
+      }
+    }
+  });
+
+  casilla33.addEventListener('click', () => {
+    if (turno) {
+      let filled = casilla33.hasChildNodes();
+      if (!filled) {
+        casilla33.innerHTML = '<img src="./img/equis1.png" style="width:100%; height:100%;">';
+        console.log('equis');
+        nuPlayer.innerHTML = '2';
+        turno = false;
+      }
+    } else {
+      let filled = casilla33.hasChildNodes();
+      if (!filled) {
+        casilla33.innerHTML = '<img src="./img/circulo1.png" style="width:100%; height:100%;">';
+        console.log('circulo');
+        nuPlayer.innerHTML = '1';
+        turno = true;
+      }
+    }
+  });
 }
 
-inicio();
 
-//tie.classList.toggle('visible');
+
+inicio();
